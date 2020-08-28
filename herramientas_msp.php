b57d0d7f4e93cf191915cfc293972113779d987a
Frank-ec <franklynlara@hotmail.com>
2020-08-28 15:23:55 -0500

    primer push

--- herramientas_msp.php
+++ herramientas_msp.php
@@ -1,30 +1,6 @@
-<!DOCTYPE html>
-<html lang= "es">
-<head>
-	<meta charset="utf-8">
-	<title>CENTRO DE SALUD TABACUNDO TIPO C</title>	
-	<link rel="stylesheet" href="css/estilos.css">
+<?php include('includes/enlaces.php') ?>
+<?php include('includes/header.php') ?>
 
-</head>
-<body>
-		<div id= "contenedor">
-			<header>		
-				<img height="65" width="250" src="img/logoMSP.gif">
-				<!--<img height="65" width="180" src="http://instituciones.msp.gob.ec/somossalud/images/recursos/BANNER-PRINC_01.png" style="float:right">-->
-				<h1>CENTRO DE SALUD TABACUNDO TIPO C</h1>
-
-				<div align="right">
-				<?php
-				//echo "la fecha actual es " . date("d") . " del " . date("m") . " de " . date("Y");
-				echo "<h5>La fecha actual es :  ". date('d-m-Y')."</h5>";
-				?>	
-
-				<h4 align="right" ><a href="./index.php"> Salir del Formulario</a></h3> 			
-				</div>
-
-
-
-			</header>	
 			<nav>
 				<div id="popup">
 				<ul class="nav">
@@ -46,7 +22,7 @@
                 
                 				<li><a href="http://181.196.107.78:7474/faces/index.xhtm/" target="_blank"><img align="center" border="3" height="50" width="80" src="img/referencias.png"><span>Sistema de Agendamiento de Turnos para Referencias y Contrarreferencias (SAT-REC)</span></a></li>         
                 											
-								<li><a href="http://172.22.67.200/admision/almacenamiento.php" ><img align="center" border="3" height="50" width="80" src="img/nube.jpg"><span>Nube Interna CST/ Nube Externa 17D10</span></a></li>
+								<li><a href="almacenamiento.php" ><img align="center" border="3" height="50" width="80" src="img/nube.jpg"><span>Nube Interna CST/ Nube Externa 17D10</span></a></li>
 								
                 				<li><a href="https://docs.google.com/spreadsheets/d/12mq6JKc05ho1CTjZenetWIYAunBYOhzStsVkPvNpywA/edit?usp=sharing" target="_blank"><img align="center" border="3" height="50" width="80" src="img/piane.png"><span>Plan Intersectorial de Alimentación y Nutrición Ecuador - PIANE</span></a></li>
                 				
@@ -58,27 +34,14 @@
 			</div>	
 			</nav>
 			</header>
-					
-
-			
-			
 		 			<div class="slider1">
-
 					<br><br><br><br><br><br>
-
-						<center><a href="http://172.22.67.200/admision/index.php"><img align="center" height="100" width="600" src="./img/herramientas_informaticas2.jpg"></a>
+						<center><a href="index.php"><img align="center" height="100" width="600" src="./img/herramientas_informaticas2.jpg"></a>
 					<br><br><br><br><br><br>
 			
 					</div>
-
-						
-	
-	
-					
 			</aside>
-	<footer>
-				<center><h2>Derechos Reservados: Centro de Salud Tabacundo Tipo C</h2>
-			</footer>
+			<?php include('includes/footer.php') ?>
 		</div>
 		
 </body>
--- index.php
+++ index.php
@@ -1,105 +1,29 @@
-<?php 
-// Url al servidor local
-$href1 ='<a href="http://172.22.67.200';
-$href2 ='<a href="http://186.47.99.108';
-$href3 ='<a href="https://dd17d10.orion-labs.com';
+<?php include('includes/enlaces.php') ?>
+<?php include('includes/header.php') ?>
 
-// Rutas a enlaces 
-$enlace1 = $href1.'/tarjetero/"><img align="center"
- 			border="3" height="50" width="80" src="./img/tarjetero.jpeg"><span>Tarjetero indice</span></a>';
-
-$enlace2 = $href1.'/triaje/"><img align="center"
- 			border="3" height="50" width="80" src="./img/triaje.png"><span>Triaje de Emergencia</span></a>';
-
-$enlace3 = $href2.'/resultados/"><img align="center"
- 			border="3" height="50" width="80" src="./img/laboratorio.png"><span>Resultados de Laboratorio</span></a>';
-
-$enlace4 = $href3.'/"><img align="center"
- 			border="3" height="50" width="80" src="./img/pedido.jpeg"><span>Pedidos de Laboratorio</span></a>';
-
-$cie10 = $href2.'/busqueda_cie10"><img align="center"
-                        border="3" height="50" width="80" src="./img/cie10.png"><span>Consulta CIE10</span></a>';
-
-$enlace5 = $href1.'/resultados/"><img align="center"
- 			border="3" height="50" width="80" src="./img/rx.jpeg"><span>Resultados de RX</span></a>';
-
-$enlace6 = $href2.'/contrareferencias/"><img align="center"
-                        border="3" height="50" width="80" src="./img/contrareferencia.jpeg"><span>Contrareferencias</span></a>';
-
-$enlace7 = $href1.'/triaje/indextv.php"><img align="center"
-                        border="3" height="50" width="80" src="./img/manchester.jpg"><span>Pacientes en Emergencia</span></a>';
-
-
-$enlace8 = $href1.'/images/index.php"><img align="center"
-                        border="3" height="50" width="80" src="./img/recortar.png"><span>Comprimir Imágenes</span></a>';
-
-
-$fondo = $href2.'/fondos/actual.jpeg" target="_blank"><img align="center" border="3" height="50" width="80"
-		src="http://186.47.99.108/fondos/actual.jpeg"><span>Fondo de Pantalla Institicional</span></a>';
- 
-$enlace9 = $href1.'/resultados/index.php/s/EBHAtgaq65E857R"><img align="center"
- 			border="3" height="50" width="80" src="./img/farmacia.jpeg"><span>Stock de Farmacia</span></a>';
-
-$enlace10 = ' ';
-
-?>
-
-<!DOCTYPE html>
-<html lang= "es">
-<head>
-	<meta charset="utf-8">
-	<title>CENTRO DE SALUD TABACUNDO TIPO C</title>	
-	<link rel="stylesheet" href="css/estilos.css">
-
-</head>
-<body>
-		<div id= "contenedor">
-			<header>		
-				<img height="65" width="250" src="img/logoMSP.gif">
-				<!--<img height="65" width="180" src="http://instituciones.msp.gob.ec/somossalud/images/recursos/BANNER-PRINC_01.png" style="float:right">-->
-				<h1>CENTRO DE SALUD TABACUNDO TIPO C</h1>
-
-				<div align="right">
-				<?php
-				//echo "la fecha actual es " . date("d") . " del " . date("m") . " de " . date("Y");
-				echo "<h5>La fecha actual es :  ". date('d-m-Y')."</h5>";
-				?>				
-				</div>
-
-			</header>	
 			<nav>
 				<div id="popup">
 				<ul class="nav">
-						
-                				<li><?php echo $enlace1 ?> </li>	
-                				<li><?php echo $enlace2 ?> </li>
-                				<li><?php echo $enlace3 ?> </li>
-                				<li><?php echo $enlace4 ?> </li>
-						 		<li><?php echo $cie10 ?> </li>
-	               				<li><?php echo $enlace5 ?> </li>
-                				<li><?php echo $enlace6 ?> </li>
-                				<li><?php echo $enlace7 ?> </li>
-                				<li><?php echo $enlace8 ?> </li>
-                				<li><?php echo $enlace9 ?> </li>
-							
-						<li><a href="http://172.22.67.200/admision/herramientas_msp.php"><img align="center" border="3" height="50" width="80" src="./img/herramientas.jpg"><span>Herramientas Informáticas MSP</span></a></li>	
-					
-<li><a href="http://172.22.67.200/resultados/index.php/s/ZYJp9AsFNAHCsqS" target="_blank"><img align="center" border="3" height="50" width="80" src="img/biblioteca1.jpg"><span>Biblioteca Virtual CST</span></a></li>
-		
-						<li><a href="consultaseguro" ><img align="center" border="3" height="50" width="80" src="./img/menu/seguros.jpg"><span>Consulta de Seguros para Planillaje</span></a></li>
-<!--<li><a href="http://172.22.67.200/admision/formularios/index.php" ><img align="center" border="3" height="50" width="80" src="./img/menu/indice.jpg"><span>Formulario para ingreso de datos.
-								</span></a></li>-->
-<li><a href="http://172.22.67.200/admision/agendaTelefonica.html" ><img align="center" border="3" height="50" width="80" src="./img/guia-telefonica.jpg"><span>Agenda Telefónica CST.
-						</span></a></li>	
-				<!--<li><a href="https://goo.gl/forms/1rhrWhgYy2X392cA2" ><img align="center" border="3" height="50" width="80" src="./img/menu/indice.jpg"><span>Formulario para la Gestión de Cuasi-eventos, Eventos Adversos y Eventos Centinela Relacionados con la Atención Clínica/Quirurgica/Administrativa.
-								</span></a></li>-->
-          				<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSdlSQaYAqcUF4ZZyXezk6z4CVSZW5XYogb1sRv5VpAW-rBtVw/viewform?usp=sf_link" ><img align="center" border="3" height="50" width="80" src="./img/control_impresoras.jpg"><span>Control de Impresoras CST.
-						</span></a></li>
-        				<li><?php echo $fondo ?> </li>
- 			</div>	
+								<li><?php echo $tarjetero ?> </li>	
+                				<li><?php echo $triaje ?> </li>
+								<li><?php echo $triajeTV ?> </li>
+                				<li><?php echo $rxResultados ?> </li>
+								<li><?php echo $stockFarmacia ?> </li>
+								<li><?php echo $biblioteca ?> </li>
+								<li><?php echo $recortar ?> </li>
+								<li><?php echo $controlImpresoras ?> </li>
+								<li><?php echo $dirTelefonico ?> </li>
+								<li><?php echo $cie10 ?> </li>
+								<li><?php echo $contrareferencias ?> </li>
+								<li><?php echo $fondo ?> </li>
+                				<li><?php echo $labPedidos ?> </li>
+                				<li><?php echo $labResultados ?> </li>
+<li><a href="herramientas_msp.php"><img align="center" border="3" height="50" width="80" src="./img/herramientas.jpg"><span>Herramientas Informáticas MSP</span></a></li>	
+<li><a href="seguros.php" ><img align="center" border="3" height="50" width="80" src="./img/seguros.jpg"><span>Consulta de Seguros para Planillaje</span></a></li>
+			</ul>	
+ 			</div>
 			</nav>
-			</header>
-				</ul>	
+					
 			<section id="contenido">
             <p style="background-color: yellow;">
             </p>
@@ -110,20 +34,8 @@ $enlace10 = ' ';
 		<ul>
 				
 					<!--<video autoplay="autoplay" height="400" width="695" src="./video/ESAMYN.mp4" ></video>-->
-
-					
 					<!--<video autoplay="autoplay" width="770" src="./video/AgitaTuMundo.mp4"></video>-->
-
-						<li><img src="./img/institucion/logoMSP.jpg"></li>
-        
-					<li><img src="./img/institucion/1.jpg" alt="" ></li>
-					<li><img src="./img/institucion/2.jpg" alt=""></li>
-					<li><img src="./img/institucion/4.jpg" alt=""></li>
-					<li><img src="./img/institucion/3.jpg" alt=""></li>
-					<li><img src="./img/institucion/5.jpg" alt=""></li>
-					<li><img src="./img/institucion/6.jpg" alt=""></li>
-					<li><img src="./img/institucion/7.jpg" alt=""></li>
-
+						<li><img src="./img/logoMSP.jpg"></li>
 					<!--<li><img src="./img/campeonato/logoMSP.JPG"></li>
 					<li><img src="./img/campeonato/1.jpg" alt="" ></li>
 					<li><img src="./img/campeonato/7.jpg" alt=""></li>
@@ -148,10 +60,6 @@ $enlace10 = ' ';
 						<center><h4>NOTAS IMPORTANTES</h4></center><br>
 						<marquee direction="up" scrollamount="1" bgcolor="white"><h3>
 									<div style="text-align:justify">
-													
-													
-													
-
 													<!--<p><font color="blue">IMPORTANTE: REALIZAR CURSO VIRTUAL</font><br>	 
 													<hr>
 													<br>
@@ -168,17 +76,8 @@ $enlace10 = ' ';
 													
 													Como ingresar:<font color="blue">Hacer clic en el link: "Cursos."</font>
 													<br><br>
-													
-
-													
-
 													Clave de matriculación: <font color="blue">trabajo.2018</font>
 													<br><br> 
-													
-												
-													
-
-
 
 													<font color="blue">
 													Atentamente:
@@ -192,14 +91,6 @@ $enlace10 = ' ';
 													C.S. Tabacundo Tipo C.
 
 													</p><br><br>-->
-													
-
-
-												
-
-
-
-					
 
 													<p><font color="blue">SIN NOTAS IMPORTANTES</font><br>	 
 													
@@ -253,16 +144,9 @@ $enlace10 = ' ';
 													</p><br><br>-->
 													<hr>
 												</div>
-
-
-
-
-
 													  </h3></marquee>
-					
 							</td>
 					</table>	
-					
 						</td>
 					</table>
 					<!--<font color="blue">1. </font>
@@ -285,10 +169,10 @@ $enlace10 = ' ';
 					<h4><a href ="http://www.salud.gob.ec/guias-de-practica-clinica/" target="_blank">Guías de Práctica Clínica</a></h4>
 					<br>
 			</aside>
-	<footer>
-				<center><h2>Derechos Reservados: Centro de Salud Tabacundo Tipo C</h2>
-			</footer>
-		</div>
+		
+		<?php include('includes/footer.php') ?>
+
+	</div>
 		
 </body>
 </html>
